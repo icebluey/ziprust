54877ec42c587328a1ebf724336622380e860081
icebluey <xylene2016@protonmail.com>
2026-02-26 12:09:29 +0000

    init

---
+++ al9/rust.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+TZ='UTC'; export TZ
+umask 022
+CC=gcc
+export CC
+CXX=g++
+export CXX
+/sbin/ldconfig
+
+set -euo pipefail
+
+rm -fr /usr/local/rust
+mkdir /usr/local/rust
+
+export CARGO_HOME=/usr/local/rust
+export RUSTUP_HOME=/usr/local/rust
+
+cd /tmp
+rm -fr /tmp/*
+curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs -o install_rust.sh
+bash install_rust.sh -v --default-host x86_64-unknown-linux-gnu --default-toolchain stable --profile complete -y
+echo ""
+ls -lah /usr/local/rust
+echo ""
+. /usr/local/rust/env
+cargo install cargo-download
+cargo install cargo-outdated
+rustup target add x86_64-pc-windows-msvc
+cargo install cargo-xwin
+echo ""
+ls -lah /usr/local/rust
+echo ""
+cd /usr/local
+_rust_ver="$(./rust/bin/rustc --version | awk '{print $2}')"
+tar -cf "rust-v${_rust_ver}-stable-x86_64-el9.tar" rust
+sleep 2
+rm -fr rust
+xz -f -z -9 -k -T$(($(nproc) - 1)) "rust-v${_rust_ver}-stable-x86_64-el9.tar"
+sleep 2
+sha256sum -b "rust-v${_rust_ver}-stable-x86_64-el9.tar".xz > "rust-v${_rust_ver}-stable-x86_64-el9.tar".xz.sha256
+rm -f "rust-v${_rust_ver}-stable-x86_64-el9.tar"
+
+rm -fr /tmp/_output
+mkdir /tmp/_output
+mv -v "rust-v${_rust_ver}-stable-x86_64-el9.tar".xz* /tmp/_output/
+
+echo ""
+echo ' done'
+echo ""
+exit
+
---
+++ run-build-al9.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+TZ='UTC'; export TZ
+umask 022
+set -e
+cd "$(dirname "$0")"
+systemctl start docker
+sleep 5
+echo
+cat /proc/cpuinfo
+echo
+if [ "$(cat /proc/cpuinfo | grep -i '^processor' | wc -l)" -gt 1 ]; then
+    #docker run --cpus="$(cat /proc/cpuinfo | grep -i '^processor' | wc -l).0" --rm --name al9 -itd almalinux:9 bash
+    docker run --cpus="$(cat /proc/cpuinfo | grep -i '^processor' | wc -l).0" --rm --name al9 -itd quay.io/almalinuxorg/almalinux:9 bash
+else
+    #docker run --rm --name al9 -itd almalinux:9 bash
+    docker run --rm --name al9 -itd quay.io/almalinuxorg/almalinux:9 bash
+fi
+sleep 2
+docker exec al9 dnf clean all
+docker exec al9 dnf makecache
+docker exec al9 dnf install -y wget bash openssl gcc g++ cmake m4 pkgconf clang llvm glibc-devel git
+docker exec al9 dnf install --allowerasing -y coreutils binutils findutils util-linux sed gawk tar xz gzip bzip2 file
+docker exec al9 /bin/bash -c 'ln -svf bash /bin/sh'
+docker exec al9 dnf update -y
+docker exec al9 /bin/bash -c 'rm -fr /tmp/*'
+docker cp al9 al9:/home/
+docker exec al9 /bin/bash /home/al9/brg.sh
+mkdir -p /tmp/_output_assets
+docker cp al9:/tmp/_output /tmp/_output_assets/
+
+exit
