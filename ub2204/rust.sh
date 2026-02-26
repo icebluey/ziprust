034c6efdf5b341de9cbc46b11c8ad5869cc9a62e
Kenneth Davidson <xylene2016@protonmail.com>
2026-02-26 09:04:06 -0500

    Update rust.sh

--- ub2204/rust.sh
+++ ub2204/rust.sh
@@ -37,6 +37,8 @@ find /usr/local/rust/toolchains/stable-x86_64-unknown-linux-gnu/bin/ -type f -ex
 echo ""
 ls -lah /usr/local/rust
 echo ""
+sed '/export PATH=/a\        export CARGO_HOME='\''.cargo'\''' -i /usr/local/rust/env
+cat /usr/local/rust/env
 cd /usr/local
 _rust_ver="$(./rust/bin/rustc --version | awk '{print $2}')"
 tar -cf "rust-v${_rust_ver}-stable-x86_64-ub2204.tar" rust
